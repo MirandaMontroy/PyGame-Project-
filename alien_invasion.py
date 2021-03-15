4f06bbcf7cf9d71d76656f45e49c4687f7f5a6aa
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-03-15 01:11:59 -0500

    settings update and code refacture

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,9 +45,17 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-        #check for any bullets that have hit aliens 
-        #if so, get rid of the bullet and the alien 
+       self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        #respond to alien collisions 
+        #remove any bullets and aliens that have collided 
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            #destroy existing bullets and create new fleet 
+            self.bullets.empty()
+            self._create_fleet
 
     def _update_aliens(self):
         #update the positions of all aliens in the fleet 
--- settings.py
+++ settings.py
@@ -10,10 +10,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         #ship speed settings 
-        self.ship_speed = 1.5
+        self.ship_speed = 1.0
 
         #bullet settings 
-        self.bullet_speed = 1.0 
+        self.bullet_speed = 1.5 
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
